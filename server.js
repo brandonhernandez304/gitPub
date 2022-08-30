48b8a3f4aefd19cc205dbdcd0dede78e6c7493ed
Brandon Hernandez <bhernandez30497@gmail.com>
2022-08-30 18:14:46 -0400

    initializes server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+//require express
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.listen(port,()=>{
+    console.log("gitpub server is listening")
+})
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Gitpub App!")
+})
